27d5592e24e81cfc7fa90582a58da541b8112739
modzilla99 <32171364+modzilla99@users.noreply.github.com>
2020-05-11 16:08:53 +0200

    Update textadventure.py

--- textadventure.py
+++ textadventure.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+
+import time
+
+class position(self, direction, room, name):
+    
+    def __init__ (self, name, direction, room):
+        self.name = name
+        self.room = room
+        self.direction = direction
+    
+    def entry (self, response):
+        return 
